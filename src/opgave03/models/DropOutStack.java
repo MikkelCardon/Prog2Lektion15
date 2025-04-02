ade6c1d99534e88bcb8e0e5d9c31bf24479dd365
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-04-02 11:55:12 +0200

    exercises done

--- src/opgave03/models/DropOutStack.java
+++ src/opgave03/models/DropOutStack.java
@@ -2,9 +2,12 @@ package opgave03.models;
 
 import opgave01.models.StackEaaa;
 
-public class DropOutStack<E> implements StackEaaa<E> {
+import java.util.NoSuchElementException;
 
+public class DropOutStack<E> implements StackEaaa<E> {
+    private Node<E> head;
     private int maxSize;
+    private int size = 0;
 
     public DropOutStack(int maxSize) {
         this.maxSize = maxSize;
@@ -12,37 +15,82 @@ public class DropOutStack<E> implements StackEaaa<E> {
 
     @Override
     public void push(E element) {
+        if (isEmpty()){
+            head = new Node<>(element);
+            size++;
+        }
+        else if (maxSize != size){
+            Node<E> newNode = new Node<>(element);
+            head.previous = newNode;
+            newNode.next = head;
+            head = newNode;
+            size++;
+        }else{
+            Node<E> currentNode = head;
+            while(currentNode.next != null){
+                currentNode = currentNode.next;
+            }
+            currentNode.previous.next = null;
 
+            Node<E> newNode = new Node<>(element);
+            head.previous = newNode;
+            newNode.next = head;
+            head = newNode;
+        }
     }
+
 
     @Override
     public E pop() {
-        return null;
+        if (isEmpty()){
+            throw new NoSuchElementException("Stack is empty");
+        }
+        if (head.next == null){
+            E e = head.element;
+            head = null;
+            size--;
+            return e;
+        }
+        E e = head.element;
+
+        head.next.previous = null;
+        head = head.next;
+        size--;
+
+        return e;
     }
 
     @Override
     public E peek() {
-        return null;
+        if (isEmpty()){
+            throw new NoSuchElementException("Stack is empty");
+        }
+        return head.element;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return head == null;
     }
 
     @Override
     public void clear() {
-
+        head = null;
+        size = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     private class Node<T> {
         T element;
         Node<T> next;
         Node<T> previous;
+
+        public Node(T element) {
+            this.element = element;
+        }
     }
 }
